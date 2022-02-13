361c6ed77039a9400b88ef30383c7d9135bcd3de
abdullah_awad <abdullah.alashafi@gmail.com>
2022-02-13 12:59:07 +0300

    add varibles

--- JPG2PDF.py
+++ JPG2PDF.py
@@ -1,6 +1,11 @@
 from PIL import Image
 
-image = Image.open('GOV_ID.jpeg')
+# jpg photo path that you want to convert 
+JPG_FILE_PATH = ''
+# path of pdf file after conveting
+PDF_FILE_PATH = ''
+
+image = Image.open(JPG_FILE_PATH)
 
 image_pdf = image.convert('RGB')
-image_pdf.save('./Govt. ID.pdf')
+image_pdf.save(PDF_FILE_PATH)
